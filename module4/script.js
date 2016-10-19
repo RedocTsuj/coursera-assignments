0c700fc767c1731f481797dfcb948076920c1334
RedocTsuj <Redoc.Tsuj@gmail.com>
2016-10-19 20:15:59 +0300

    Final submission for Module 4 assignment

--- module4/script.js
+++ module4/script.js
@@ -34,32 +34,49 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+console.log("======= Assignment Output Starts after this line ========");
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using the 'speak' method or either helloSpeaker's or byeSpeaker's
-// 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using the 'speak' method or either helloSpeaker's or byeSpeaker's
+  // 'speak' method.
+  // See Lecture 50, part 1
+  for (var i in names) {
+    var name=names[i];
+    // console.log(name);
+    
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    var firstLetter =name.charAt(0).toLowerCase();
+    // console.log(firstLetter);
 
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    var mySpeak;
+    if (firstLetter == "j") {
+      mySpeak=byeSpeaker.speak;
+    } else {
+      mySpeak=helloSpeaker.speak;
+    }
+    mySpeak(name);
   }
-}
+  
+})();
+
+console.log("======= Assignment Output Finished =====================");
+// and an additional flavor : display the value of output variable inside and HTML element
+// the variable value has been constructed every time either speak function is called (see implementation of each speak function)
+document.getElementById("webconsole").innerHTML=output;
+console.log("Note from Panagiotis : Thanks for grading this !");
+console.log("========================================================");
